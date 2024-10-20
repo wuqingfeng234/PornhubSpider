6e7827d8f5fdd286bdc722fd5498cc97e64e70df
wuqf <wuqingfeng234@163.com>
2024-10-20 21:00:11 +0800

    add proxy

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import re,requests,time,os
+import re, requests, time, os
 from lxml import etree
 
 url_sta = "https://cn.pornhub.com"
+proxy = {'http': 'http://127.0.0.1:7890', 'https': 'http://127.0.0.1:7890'}
 url_list = []
 title_list = []
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36"}
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36"}
+
 
 def Mkdir(path):
     path = path.strip()
@@ -19,20 +22,24 @@ def Mkdir(path):
     else:
         pass
 
+
 def FileDel():
     os.remove('url.txt')
 
+
 def Read_file(filepath):
     with open(filepath) as fp:
-        content=fp.read()
+        content = fp.read()
     return content
 
+
 def Get_Page(x):
-    Page = requests.get(x,headers=headers)
+    Page = requests.get(x, headers=headers, proxies=proxy)
     Page.encoding = "utf-8"
     Page = Page.text
     Page = etree.HTML(Page)
     return Page
+
 
 def Get_Url(x):
     if 'search?search=' in x:
@@ -44,53 +51,57 @@ def Get_Url(x):
     for i in range(1, len(VideoUrl)):
         url_list.append(url_sta + VideoUrl[i])
     for i in range(len(VideoTitle)):
-        title = re.sub("[\n\t\\\/:*?,，!！？。()（ ）.\"<=->|\]\[]","",VideoTitle[i])
+        title = re.sub("[\n\t\\\/:*?,，!！？。()（ ）.\"<=->|\]\[]", "", VideoTitle[i])
         title_list.append(title)
+
 
 def Get_Video_Link(x):
     while True:
         JS_File = Get_Page(x).xpath('//*[@id="player"]/script[1]/text()')
-        JS = re.sub('[\n\t]','',JS_File[0])
-        voll = re.findall('var player_mp4_seek = "ms";(.*?)flashvars',JS)  #ms/start
+        JS = re.sub('[\n\t]', '', JS_File[0])
+        voll = re.findall('var player_mp4_seek = "ms";(.*?)flashvars', JS)  # ms/start
         try:
             if voll[0]:
                 break
         except:
             continue
-    var_start =  re.sub('var ','',voll[0])
-    var_end =  re.sub('/\*(.*?)\*/','',var_start)
-    sta = re.findall('(.*?)quality_',var_end)
-    end = re.findall('quality_\d{3,4}p=(.*?);',var_end)
-    WriteUrl = sta[0]+'\n'+'url = '+end[0]+'\nfile_handle=open("url.txt",mode="w")'+\
-               '\nfile_handle.write(url)'+'\nfile_handle.close()'
+    var_start = re.sub('var ', '', voll[0])
+    var_end = re.sub('/\*(.*?)\*/', '', var_start)
+    sta = re.findall('(.*?)quality_', var_end)
+    end = re.findall('quality_\d{3,4}p=(.*?);', var_end)
+    WriteUrl = sta[0] + '\n' + 'url = ' + end[0] + '\nfile_handle=open("url.txt",mode="w")' + \
+               '\nfile_handle.write(url)' + '\nfile_handle.close()'
     exec(WriteUrl)
+
 
 def Get_Img(x):
     ImgUrl = Get_Page(x).xpath('/html/head/meta[19]/@content')
-    save_img = requests.get(ImgUrl[0], headers=headers)
+    save_img = requests.get(ImgUrl[0], headers=headers, proxies=proxy)
     with open(r"Pornhub/" + Title + "/" + 'cover' + ".jpg", "wb") as fh:
         fh.write(save_img.content)
         fh.flush()
+
 
 def Download():
     DVLink = Read_file('url.txt')
     FileDel()
     NowTime = time.strftime("%Y%m%d", time.localtime())
-    r = requests.get(DVLink, stream=True)
+    r = requests.get(DVLink, stream=True, proxies=proxy)
     f = open(r"Pornhub/" + Title + "/" + NowTime + ".mp4", "wb")
     for chunk in r.iter_content(chunk_size=1024):
-       if chunk:
-           f.write(chunk)
+        if chunk:
+            f.write(chunk)
+
 
 def cs1():
     global Title
     url = input("Video Url:")
-    url = re.sub(" ","",url)
+    url = re.sub(" ", "", url)
     Get_Video_Link(url)
     Title = Get_Page(url).xpath('//*[@class="inlineFree"]/text()')
     Title = re.sub("[\n\t\\\/:*?,，!！？。()（ ）.\"<=->|\]\[]", "", Title[0])
     Path = "Pornhub/" + Title + "/"
-    print('标题：'+Title + '\n' + Read_file('url.txt') + '\n')
+    print('标题：' + Title + '\n' + Read_file('url.txt') + '\n')
     Download_Chioce = input("是否下载当前视频？(y/n)")
     if Download_Chioce == 'y' or Download_Chioce == 'Y':
         Mkdir(Path)
@@ -99,12 +110,13 @@ def cs1():
     else:
         pass
 
+
 def cs2():
     global Title
     url = input("Page Url:")
-    url = re.sub(" ","",url)
+    url = re.sub(" ", "", url)
     Get_Url(url)
-    Download_Chioce = input("当前页面共有%d个视频，是否全部下载？(y/n)"%len(url_list))
+    Download_Chioce = input("当前页面共有%d个视频，是否全部下载？(y/n)" % len(url_list))
     if Download_Chioce == 'y' or Download_Chioce == 'Y':
         for i in range(len(url_list)):
             try:
@@ -112,7 +124,7 @@ def cs2():
                 Get_Video_Link(url)
                 Title = re.sub("[\n\t\\\/:*?,，!！？。()（ ）.\"<=->|\]\[]", "", title_list[i])
                 Path = "Pornhub/" + Title + "/"
-                print('标题：'+Title+'\n'+Read_file('url.txt')+'\n')
+                print('标题：' + Title + '\n' + Read_file('url.txt') + '\n')
                 Mkdir(Path)
                 Get_Img(url)
                 Download()
@@ -130,4 +142,6 @@ def start():
         cs2()
     else:
         print("输入有误")
+
+
 start()
